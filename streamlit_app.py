a5c277249d144041ea11b35dafea1e7205f950e0
longdange <133305164+longdange@users.noreply.github.com>
2025-08-05 10:02:05 +0900

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="DBTI 대시보드", page_icon="🐶")
+
+st.title("🐶 반려동물 성향 분석 플랫폼")
+st.markdown("""
+👈 왼쪽 사이드바에서 기능을 선택해보세요!
+
+- DBTI 분석 보기
+- MBTI 기반 동물 궁합 추천
+""")
